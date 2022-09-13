3b5ba66763315dce1ded9e9e7a8dafb6ac29a926
michaelrios2015 <michael.rios093@gmail.com>
2022-09-12 21:19:22 -0400

    yeah I am confused

--- chapter3/forkexec.cpp
+++ chapter3/forkexec.cpp
@@ -3,19 +3,25 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
     pid_t pid;
-    
+
     /* fork a child process */
 
     pid = fork();
-    if (pid < 0) { /* error occurred */
+    if (pid < 0)
+    { /* error occurred */
         fprintf(stderr, "Fork Failed");
         return 1;
-    } else if (pid == 0) { /* child process */
-        execlp("/bin/ls","ls","-l", NULL);
-    } else { /* parent process */
+    }
+    else if (pid == 0)
+    { /* child process */
+        // execute a file?? like
+        execlp("/bin/ls", "ls", "-l", NULL);
+    }
+    else
+    { /* parent process */
         /* parent will wait for the child to complete */
         wait(NULL);
         printf("Child Complete");
